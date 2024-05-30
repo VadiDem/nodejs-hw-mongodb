36f716024a28d142b4d2a828af8d2aca9a552f13
VadiDem <demochko.vadim@gmail.com>
2024-05-30 22:23:28 +0300

    fin hw4

--- src/services/contacts.js
+++ src/services/contacts.js
@@ -1,9 +1,29 @@
 import { ContactsCollection } from "../db/models/contact.js";
+import { calculatePaginationData } from "../utils/calculatePaginationData.js";
+import { SORT_ORDER } from "../constans/index.js";
+import { parseIsFavourite } from "../utils/parseFilterParams.js";
 
+export const getAllContacts = async (page=1, perPage=10, sortBy = 'name', sortOrder = SORT_ORDER.ASC, filter = {}) => {
+   const limit = perPage;
+   const skip = (page - 1) * perPage;
 
-export const getAllContacts = async () => {
-    const contacts = await ContactsCollection.find();
-    return contacts;
+    let contactsQuery = ContactsCollection.find().skip(skip).limit(limit).sort({ [sortBy]: sortOrder });
+
+      if (filter.isFavourite !== undefined) {
+        const parsedIsFavourite = parseIsFavourite(filter.isFavourite);
+        if (parsedIsFavourite !== undefined) {
+            contactsQuery = contactsQuery.where('isFavourite').equals(parsedIsFavourite);
+        }
+    }
+
+  const contactsCount = await ContactsCollection.countDocuments();
+  const contacts = await contactsQuery.exec();
+  const paginationData = calculatePaginationData(contactsCount, limit, page);
+
+  return {
+    data: contacts,
+    ...paginationData,
+  };
 };
 
 export const getContactsById = async (contactId) => {
