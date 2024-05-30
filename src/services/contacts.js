12e3eca22b13121617eb2b1433530052274e28b7
VadiDem <demochko.vadim@gmail.com>
2024-05-30 18:07:59 +0300

    fin hw4

---
+++ src/services/contacts.js
@@ -0,0 +1,64 @@
+import { ContactsCollection } from "../db/models/contact.js";
+import { calculatePaginationData } from "../utils/calculatePaginationData.js";
+import { SORT_ORDER } from "../constans/index.js";
+import { parseIsFavourite } from "../utils/parseFilterParams.js";
+
+export const getAllContacts = async (page=1, perPage=10, sortBy = 'name', sortOrder = SORT_ORDER.ASC, filter = {}) => {
+   const limit = perPage;
+   const skip = (page - 1) * perPage;
+
+    let contactsQuery = ContactsCollection.find().skip(skip).limit(limit).sort({ [sortBy]: sortOrder });
+
+      if (filter.isFavourite !== undefined) {
+        const parsedIsFavourite = parseIsFavourite(filter.isFavourite);
+        if (parsedIsFavourite !== undefined) {
+            contactsQuery = contactsQuery.where('isFavourite').equals(parsedIsFavourite);
+        }
+    }
+
+  const contactsCount = await ContactsCollection.countDocuments();
+  const contacts = await contactsQuery.exec();
+  const paginationData = calculatePaginationData(contactsCount, limit, page);
+
+  return {
+    data: contacts,
+    ...paginationData,
+  };
+};
+
+export const getContactsById = async (contactId) => {
+    const contact = await ContactsCollection.findById(contactId);
+    return contact;
+};
+
+export const createContacts = async (payload) => {
+    const contact = await ContactsCollection.create(payload);
+    return contact;
+};
+
+export const deleteContact = async (contactId) => {
+    const contact = await ContactsCollection.findOneAndDelete({
+        _id: contactId,
+    });
+
+    return contact;
+};
+
+export const updateContact = async (contactId, payload, options = {}) => {
+
+    const rawResult = await ContactsCollection.findOneAndUpdate(
+        { _id: contactId },
+        payload,
+        {
+        new: true,
+        includeResultMetadata: true,
+        ...options,
+    },);
+
+    if (!rawResult || !rawResult.value) return null;
+
+    return {
+        contact: rawResult.value,
+        isNew: Boolean(rawResult?.lastErrorObject?.upserted),
+    };
+};
