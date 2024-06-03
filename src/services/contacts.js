81025114deec0efd72f26ae2319a70e6407488d0
VadiDem <demochko.vadim@gmail.com>
2024-06-03 16:50:31 +0300

    fix 33

--- src/services/contacts.js
+++ src/services/contacts.js
@@ -3,9 +3,9 @@ import { ContactsCollection } from "../db/models/contact.js";
 export const getAllContacts = async () => {
     const contacts = await ContactsCollection.find();
     return contacts;
-}
+};
 
 export const getContactsById = async (contactId) => {
     const contact = await ContactsCollection.findById(contactId);
     return contact;
-}
+};
